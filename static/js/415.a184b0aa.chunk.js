2aaa3e257d908cf5b2d96cd599108240629f3ed2
PavelKreitsberg <99120090+PavelKreitsberg@users.noreply.github.com>
2022-12-12 14:15:53 +0000

    Deploying to gh-pages from @ PavelKreitsberg/goit-react-hw-08-phonebook@44481e60bff89edb4347293cdef6069ecf2b0544 🚀

---
+++ static/js/415.a184b0aa.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{415:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});var n=r(184);function c(){return(0,n.jsx)("div",{children:(0,n.jsx)("h2",{children:"Welcome to our App"})})}}}]);
+//# sourceMappingURL=415.a184b0aa.chunk.js.map
